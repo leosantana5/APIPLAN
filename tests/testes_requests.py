72a9f2ebfbadaf33aafbc3348c0f9bbe5aa64ad0
leosantana5 <64759399+leosantana5@users.noreply.github.com>
2025-07-10 15:19:31 -0300

    update

--- tests/testes_requests.py
+++ tests/testes_requests.py
@@ -5,7 +5,8 @@ from fastapi.responses import StreamingResponse
 from io import BytesIO
 
 
-BASE_URL = "http://127.0.0.1:8000"
+# BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "https://apiplan-qw64.onrender.com"
 
 
 
